d518742bf33f64e54496a1b81da4cafb50c5d26a
dsNiki <nikidienes195@gmail.com>
2026-01-08 20:22:35 +0100

    Frontend-only for Vercel

--- backend/config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-class Config:
-    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://user:password@db:3306/studybuddy"
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = "super-secret-key"
